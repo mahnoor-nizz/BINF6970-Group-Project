c5e8357ef2b2ef8109d6d37defeb8d6250cf98e6
chrysle28 <kenneth.gamueda@gmail.com>
2026-03-10 18:08:31 -0400

    Updated alpha_search function

--- stats_q2.R
+++ stats_q2.R
@@ -8,13 +8,14 @@ library(MASS)
 
 # === 1 | DATA CLEANING AND EDA ========
 COV <- read_excel("Immunologic profiles of patients with COVID-19.xlsx")
-
 View(COV)
 str(COV)
 
 #dataset contains measurements of 28 analytes (cytokines, chemokines, and acute phase proteins) from plasma samples of 76 COVID-19-positive patients. Response variable: disease progression/severity (binary, mild vs. severe)
 
 table(COV$Severirty)
+
+## DO SOME EDA AND CHECK FOR MISSING VALUES (there should be none)
 
 # Convert severity to a factor variable
 #Severity <- ifelse(COV$Severirty == "Severe", 1, 0)
@@ -37,12 +38,13 @@ colnames(COV)
 
 # Feature Engineering ?????
 
-
+# === 2.1 | MODEL MATRIX AND TRAINING/TEST SETS ========
 # Create model matrix for glmnet including all effects (even quadratic?)
-f0 <- lm(Severity ~ ., data = COV)
+#f0 <- lm(Severity ~ ., data = COV)
 #f0 <- lm(Severity ~ .^2, data = COV)
 
-X <- model.matrix(f0)[, -1]
+X <- model.matrix(Severity ~., COV)[, -1]
+#X <- model.matrix(Severity ~.^2, COV)[, -1]
 dim(X)
 colnames(X)
 # X <- cbind(X,X[, 1:29]^2)
@@ -70,18 +72,21 @@ Y_test <- Y[test_index]
 table(Y_train)
 table(Y_test)
 
-# Grid search for optimal alpha value
-# Perform grid search (modify to take a different n-value for cross validation)
+# === 2.2 | GRID SEARCH FOR ALPHA ========
+# Function to perform grid search for optimal alpha value with different n-folds 
 alpha_search <- function (nfolds){
   alpha_grid <- seq(0, 1, .01)
   results <- data.frame()
+  set.seed(1717)
   for (a in alpha_grid) {
-  # Cross-validation for current alpha; accuracy measure is deviance since AUC could not be used due ot small number of observations per fold
-  cv_model <- cv.glmnet(X_train, Y_train, 
+  cv_model <- cv.glmnet(X_train, 
+                        Y_train, 
                         alpha = a, 
                         nfolds = nfolds,
                         family = "binomial",
-                        type.measure = "deviance") 
+                        #foldid check if needed
+                        type.measure = "deviance",
+                        keep = T)  # Accuracy measure is deviance because AUC could not be used; number of observations is too small per fold
                         #keep = TRUE) # check if keep should be T or F
   
   # Store results
@@ -95,7 +100,7 @@ alpha_search <- function (nfolds){
   }
   return(results)
 }
-## Collect and print best alpha values automatically
+## Add part to function that collects and prints best alpha values automatically
 
 alpha_10 <- alpha_search(10)
 best_min_10 <- alpha_10$alpha[which.min(alpha_10$cvm_min)]
@@ -135,15 +140,13 @@ ggplot(alpha_20, aes(x = alpha)) +
 cv_10 <- cv.glmnet(X_train, Y_train, nfolds = 10, family = "binomial", alpha = best_min_10, type.measure = "deviance")
 plot(cv_10)
 
-prd_train <- predict(cv_10,newx = X_train, type = "response", s = cv_10$lambda.min)[, 1]
-prd_test <- predict(cv_10,newx = X_test, type = "response", s = cv_10$lambda.min)[, 1]
+prd_train <- predict(cv_10, newx = X_train, type = "response", s = cv_10$lambda.min)[, 1]
+prd_test <- predict(cv_10, newx = X_test, type = "response", s = cv_10$lambda.min)[, 1]
 
 # Testing prediction accuracy (Change plots to be made with ggplot and overlay curves)
 par(mfrow=c(1,2))
-
 AUC_train <- roc(Y_train, prd_train)
 plot(AUC_train)
-
 AUC_test <- roc(Y_test, prd_test)
 plot(AUC_test)
 
@@ -152,7 +155,7 @@ max(cv_10$cvm)
 which.max(cv_10$cvm)
 cv_10$cvlo[which.max(cv_10$cvm)];cv_10$cvup[which.max(cv_10$cvm)]
 
-# Determining cutoffs for sensitivity, specificity, and classification; make function to test both
+# Determining cutoffs for sensitivity, specificity, and classification; make function to test both; return dataframe with all
 cutoffs_snsp <- function(AUC) {
   snsp <- cbind(AUC$sensitivities, AUC$specificities)
   indx <- which.max(apply(snsp, 1, min))
@@ -161,6 +164,7 @@ cutoffs_snsp <- function(AUC) {
 }
 
 cutoffs_snsp(AUC_train)
+cutoffs_snsp(AUC_test)
 # Make confusion matrix?
 
 # Training set
@@ -213,9 +217,12 @@ names(coef.min[coef.min!=0])
 # Provide ranking of top predictors
 
 # Elastic net model with 20 folds
+## Make function that does all the same steps as 10-fold
 cv_20 <- cv.glmnet(X_train, Y_train, nfolds = 20, family = "binomial", alpha = best_min_20, type.measure = "deviance")
 plot(cv_20)
 
 coef(cv_20, s = cv_20$lambda.min)
 coef.min <- coef(cv_20,s = cv_20$lambda.min)[-1,1]
 coef.min[coef.min!=0]
+
+## FOR MS. MAHNOOR NIZAMANI: Make some plots and do some analysis for how AGE correlates with severity and also which cytokines are most effective and most significant
