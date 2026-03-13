ca781be401ea10166acd3d91f4289ab3b1d24476
Kenneth Gamueda <kenneth.gamueda@gmail.com>
2026-03-13 15:49:10 -0400

    Updated models and some plots; please see comments marked with MN

--- stats_q2.R
+++ stats_q2.R
@@ -18,9 +18,8 @@ str(COV) # dataset contains measurements of 28 analytes from 76 COVID-19-positiv
 sum(is.na(COV)) # no missing values
 
 # Convert severity to a factor variable
-# COV$Severity <- ifelse(COV$Severirty == "Severe", 1, 0)
 COV$Severity <- as.factor(COV$Severirty)
-table(COV$Severity) # 0 = mild, 1 = severe
+table(COV$Severity) # 32 mild, 44 severe
 
 # Remove Patient Number and Severirty
 COV <- COV[, !(names(COV) %in% c("Severirty", "Patient Number"))]
@@ -34,6 +33,7 @@ dim(COV)       # 76 x 31
 colnames(COV)  
 
 # === 1.2| EXPLORATORY DATA ANALYSIS ========
+# For MN: can you add the EDA plots here
 
 
 # === 2.1 | MODEL MATRIX AND TRAINING/TEST SETS ========
@@ -52,26 +52,16 @@ plot(as.ts(apply(X, 2, sd)), xlab = "Covariate", ylab = "SD",
 
 # Create train (75%) & test (25%) sets
 set.seed(1717)
-test_index <- sample.int(nrow(X), round(nrow(X) * 0.25), replace = FALSE)
-X_train <- X[-test_index, ]
-Y_train <- Y[-test_index]
-X_test <- X[test_index, ]
-Y_test <- Y[test_index]
-
-# Change to data partition maybe
-set.seed(1717)
 train_index <- createDataPartition(Y, p = 0.75, list = FALSE)
-X_train2 <- X[train_index, ]
-Y_train2 <- Y[train_index]
-X_test2 <- X[-train_index, ]
-Y_test2 <- Y[-train_index]
+X_train <- X[train_index, ]
+Y_train <- Y[train_index]
+X_test <- X[-train_index, ]
+Y_test <- Y[-train_index]
 
 ## Confirm class balance in train and test
-table(Y_train)
-table(Y_test)
+table(Y_train) # 57 total, 42% in mild, 58% in severe
+table(Y_test) # 19 total, 42% in mild, 58% in severe
 
-table(Y_train2)
-table(Y_test2)
 
 # === 2.2 | GRID SEARCH FOR ALPHA ========
 # Function to perform grid search for optimal alpha value with different n-folds 
@@ -87,10 +77,8 @@ alpha_search <- function (X, Y, nfolds){
                         family = "binomial",
                         keep = T,
                         standardize = T,
-                        foldid = sample(rep(1:nfolds, length.out = length(Y))),
+                        foldid = sample(rep(1:nfolds, length.out = nrow(X))),
                         type.measure = "deviance")  # Accuracy measure is deviance because AUC could not be used; number of observations is too small per fold
-
-  # TO-DO: change to keep = T, maybe add foldid
   
   # Store results
   results <- rbind(results, data.frame(
@@ -111,25 +99,16 @@ alpha_search <- function (X, Y, nfolds){
 }
 
 # Perform grid search and obtain optimal alpha for each k-fold CV
-#alpha_10 <- alpha_search(X_train, Y_train, 10)
-#best_min_10 <- alpha_10$best_min
-#best_1se_10 <- alpha_10$best_1se
-
-alpha_10 <- alpha_search(X_train2, Y_train2, 10)
+alpha_10 <- alpha_search(X_train, Y_train, 10)
 best_min_10 <- alpha_10$best_min
 best_1se_10 <- alpha_10$best_1se
 
-#alpha_20 <- alpha_search(20)
-#best_min_20 <- alpha_20$best_min
-#best_1se_20 <- alpha_20$best_1se
-
-alpha_20 <- alpha_search(X_train2, Y_train2, 20)
+alpha_20 <- alpha_search(X_train, Y_train, 20)
 best_min_20 <- alpha_20$best_min
 best_1se_20 <- alpha_20$best_1se
 
-
 # Visualize alpha grid search to decide between alpha values
-ggplot(alpha_10$results[-1, ], aes(x = alpha)) +
+ggplot(alpha_10$results, aes(x = alpha)) +
   geom_line(aes(y = cvm_min, color = "lambda.min"), linewidth = 1.2) +
   geom_line(aes(y = cvm_1se, color = "lambda.1se"), linewidth = 1.2) +
   geom_point(aes(y = cvm_min, color = "lambda.min"), size = 3) +
@@ -157,79 +136,109 @@ ggplot(alpha_20$results, aes(x = alpha)) +
 get_cutoff <- function(AUC) {
   snsp <- cbind(AUC$sensitivities, AUC$specificities)
   indx <- which.max(apply(snsp, 1, min))
-  return(data.frame(cutoff = AUC$threshold[indx],
-                    sensitivity = snsp[indx, 1],
-                    specificity = snsp[indx, 2]))
-} # get_metrics already gets sn and sp, so might be redundant for this function
-# I agree I had it in there before I changed the get metrics function
+  return(data.frame(sensitivity = snsp[indx, 1],
+                    specificity = snsp[indx, 2],
+                    cutoff = AUC$threshold[indx]))
+} 
 
-# Function to obtain metrics from confusion matrix
-get_metrics <- function(mat) {
-  sn <- mat[2, 2] / sum(mat[2, ])
-  sp <- mat[1, 1] / sum(mat[1, ])
-  acc <- (mat[1,1] + mat[2,2]) / sum(mat)
+# Function to obtain metrics from confusion matrix; positive is set to second factor
+get_metrics <- function(mat, pos = 2) {
+  neg <- if (pos == 2) 1 else 2
+  sn <- mat[pos, pos] / sum(mat[pos, ])
+  sp <- mat[neg, neg] / sum(mat[neg, ])
+  acc <- (mat[neg, neg] + mat[pos, pos]) / sum(mat)
   return(unlist(list(sensitivity = sn, specificity = sp, accuracy = acc)))
 }
 
 # === 3.1 | ELASTIC NET MODEL (10-FOLD) ========
 set.seed(1717)
-cv_10 <- cv.glmnet(X_train2, Y_train2, nfolds = 10, family = "binomial", alpha = best_min_10, type.measure = "deviance")
+cv_10 <- cv.glmnet(X_train, Y_train, nfolds = 10, family = "binomial", alpha = best_min_10, type.measure = "deviance")
 plot(cv_10)
 title(main = paste0("10-Fold CV (alpha = ", best_min_10, ")"), line = 2.3)
 
-prd_train_10 <- predict(cv_10, newx = X_train2, type = "response", s = cv_10$lambda.min)[, 1]
-prd_test_10 <- predict(cv_10, newx = X_test2, type = "response", s = cv_10$lambda.min)[, 1]
+prd_train_10 <- predict(cv_10, newx = X_train, type = "response", s = cv_10$lambda.min)[, 1]
+prd_test_10 <- predict(cv_10, newx = X_test, type = "response", s = cv_10$lambda.min)[, 1]
 
 # Test prediction accuracy with AUC-ROC curves(Change plots to be made with ggplot and overlay curves)
-AUC_train_10 <- roc(Y_train2, prd_train_10)
-AUC_test_10  <- roc(Y_test2,  prd_test_10)
+AUC_train_10 <- roc(Y_train, prd_train_10)
+AUC_test_10  <- roc(Y_test,  prd_test_10)
 auc(AUC_train_10)
 auc(AUC_test_10)
 
 # Determine cutoffs for model with 10-folds
 cut_train_10 <- get_cutoff(AUC_train_10)
-cut_test_10 <- get_cutoff(AUC_test_10)
-
 cut_train_10
+cut_test_10 <- get_cutoff(AUC_test_10)
 cut_test_10
 
 # Make confusion matrix for model
-conf_10 <- table(y = Y_test2, yhat = as.numeric(prd_test_10 > cut_test_10$cutoff))
+conf_10 <- table(y = Y_test, yhat = as.numeric(prd_test_10 > cut_test_10$cutoff))
 conf_10
-get_metrics(conf_10)
+metrics_10 <- get_metrics(conf_10)
+metrics_10
 
-# Plot ROC curve with optimal cutoff; TO-DO: make ggplot versions (change color and linewidth and stuff)
-plot(AUC_train_10, main  = "ROC Curve - Training Set (10-Fold) with Optimal Cutoff")
-abline(h = cut_train_10$sensitivity, v = cut_train_10$specificity, col = "blue", lty = 2)
-
-plot(AUC_test_10, main = "ROC Curve - Test Set (10-Fold) with Optimal Cutoff")
-abline(h = cut_test_10$sensitivity, v = cut_test_10$specificity, col = "blue", lty = 2)
-
-ggroc(AUC_train_10, linewidth = 1.5) +
+# Plot ROC curve with optimal cutoff
+# Training set
+ggroc(AUC_train_10, linewidth = 1.1) +
   labs(title = "ROC Curve - Training Set (10-Fold) with Optimal Cutoff",
        x = "False Positive Rate (Specificity)",
        y = "True Positive Rate (Sensitivity)") +
   geom_abline(slope = 1, intercept = 1, 
-               linetype = "dashed",
                color = "grey") +
-  geom_hline(yintercept = cut_train_10$specificity,
+  geom_hline(yintercept = cut_train_10$sensitivity,
              linetype = "dashed",
              color = "blue") + 
-  geom_vline(xintercept = cut_train_10$sensitivity,
+  geom_vline(xintercept = cut_train_10$specificity,
              linetype = "dashed",
              color = "blue") +
-  theme_minimal() 
-# add labels, title, either put cutoffs on graph or legend for sn and sp, maybe even put area under curve; could also put both curves in one graph
+  annotate("text", 
+           x = 0.84,
+           y = 0.25,
+           label = "paste(Specificity, \" = 0.917\")", parse = T) +
+  annotate("text", 
+           x = 0.25,
+           y = 0.93,
+           label = "paste(Sensitivity, \" = 0.970\")", parse = T) +
+  theme_minimal() +
+  theme(plot.title = element_text(face = "bold", size = 16, hjust = 0.5),
+        axis.title.x = element_text(margin = margin(t = 10)),
+        axis.title.y = element_text(margin = margin(r = 10))) 
+
+# Test set
+ggroc(AUC_test_10, linewidth = 1.1) +
+  labs(title = "ROC Curve - Test Set (10-Fold) with Optimal Cutoff",
+       x = "False Positive Rate (Specificity)",
+       y = "True Positive Rate (Sensitivity)") +
+  geom_abline(slope = 1, intercept = 1, 
+              color = "grey") +
+  geom_hline(yintercept = cut_test_10$sensitivity,
+             linetype = "dashed",
+             color = "blue") + 
+  geom_vline(xintercept = cut_test_10$specificity,
+             linetype = "dashed",
+             color = "blue") +
+  annotate("text", 
+           x = 0.8,
+           y = 0.3,
+           label = "paste(Specificity, \" = 0.875\")", parse = T) +
+  annotate("text", 
+           x = 0.125,
+           y = 0.78,
+           label = "paste(Sensitivity, \" = 0.818\")", parse = T) +
+  theme_minimal() +
+  theme(plot.title = element_text(face = "bold", size = 16, hjust = 0.5),
+        axis.title.x = element_text(margin = margin(t = 10)),
+        axis.title.y = element_text(margin = margin(r = 10))) 
 
 # Obtain most significant coefficients as predicted by model
 coef(cv_10, s = cv_10$lambda.min)
 coef_10_min <- coef(cv_10,s = cv_10$lambda.min)[-1,1]
 coef_10_min[coef_10_min!=0] 
+# With 10-fold CV and using min for alpha and lambda, elastic net regression selected 9 relevant variables; 7 are negative and 2 are positive
 
 selected_10  <- sort(abs(coef_10_min[coef_10_min != 0]), decreasing = TRUE)
 selected_10 
-# With 10-fold CV and using min for alpha and lambda, elastic net regression selected 12 relevant variables
-# Notes: AGE has the most influence; all are positive so increasing these by 1 unit increases severity by the corresponding coefficient value 
+# TNF-a has the most influence
 
 # Ranking of top predictors TO-DO: make ggplot version
 par(mar = c(9, 4, 4, 2))
@@ -240,16 +249,16 @@ par(mar = c(5, 4, 4, 2))
 
 # === 3.2 | ELASTIC NET MODEL (20-FOLD) ========
 set.seed(1717)
-cv_20 <- cv.glmnet(X_train2, Y_train2, nfolds = 20, family = "binomial", alpha = best_min_20, type.measure = "deviance")
+cv_20 <- cv.glmnet(X_train, Y_train, nfolds = 20, family = "binomial", alpha = best_min_20, type.measure = "deviance")
 plot(cv_20)
 title(main = paste0("20-Fold CV (alpha = ", best_min_20, ")"), line = 2.5)
 
-prd_train_20 <- predict(cv_20, newx = X_train2, type = "response", s = cv_20$lambda.min)[, 1]
-prd_test_20  <- predict(cv_20, newx = X_test2,  type = "response", s = cv_20$lambda.min)[, 1]
+prd_train_20 <- predict(cv_20, newx = X_train, type = "response", s = cv_20$lambda.min)[, 1]
+prd_test_20  <- predict(cv_20, newx = X_test,  type = "response", s = cv_20$lambda.min)[, 1]
 
 # Test prediction accuracy with AUC-ROC curves
-AUC_train_20 <- roc(Y_train2, prd_train_20)
-AUC_test_20  <- roc(Y_test2,  prd_test_20)
+AUC_train_20 <- roc(Y_train, prd_train_20)
+AUC_test_20  <- roc(Y_test,  prd_test_20)
 auc(AUC_train_20)
 auc(AUC_test_20)
 
@@ -260,35 +269,72 @@ cut_train_20
 cut_test_20
 
 # Make confusion matrix for model
-conf_20 <- table(y = Y_test2, yhat = as.numeric(prd_test_20 > cut_test_20$cutoff))
+conf_20 <- table(y = Y_test, yhat = as.numeric(prd_test_20 > cut_test_20$cutoff))
 conf_20
 get_metrics(conf_20)
 
 # Plot ROC curve with optimal cutoff
-ggroc(AUC_train_20, linewidth = 1.5) +
+# Training set
+ggroc(AUC_train_20, linewidth = 1.1) +
   labs(title = "ROC Curve - Training Set (20-Fold) with Optimal Cutoff",
        x = "False Positive Rate (Specificity)",
        y = "True Positive Rate (Sensitivity)") +
   geom_abline(slope = 1, intercept = 1, 
-              linetype = "dashed",
               color = "grey") +
-  geom_hline(yintercept = cut_train_20$specificity,
+  geom_hline(yintercept = cut_train_20$sensitivity,
              linetype = "dashed",
              color = "blue") + 
-  geom_vline(xintercept = cut_train_20$sensitivity,
+  geom_vline(xintercept = cut_train_20$specificity,
              linetype = "dashed",
              color = "blue") +
-  theme_minimal() 
+  annotate("text", 
+           x = 0.84,
+           y = 0.25,
+           label = "paste(Specificity, \" = 0.917\")", parse = T) +
+  annotate("text", 
+           x = 0.25,
+           y = 0.84,
+           label = "paste(Sensitivity, \" = 0.879\")", parse = T) +
+  theme_minimal() +
+  theme(plot.title = element_text(face = "bold", size = 16, hjust = 0.5),
+        axis.title.x = element_text(margin = margin(t = 10)),
+        axis.title.y = element_text(margin = margin(r = 10))) 
+
+# Test set
+ggroc(AUC_test_20, linewidth = 1.1) +
+  labs(title = "ROC Curve - Test Set (20-Fold) with Optimal Cutoff",
+       x = "False Positive Rate (Specificity)",
+       y = "True Positive Rate (Sensitivity)") +
+  geom_abline(slope = 1, intercept = 1, 
+              color = "grey") +
+  geom_hline(yintercept = cut_test_20$sensitivity,
+             linetype = "dashed",
+             color = "blue") + 
+  geom_vline(xintercept = cut_test_20$specificity,
+             linetype = "dashed",
+             color = "blue") +
+  annotate("text", 
+           x = 0.8,
+           y = 0.3,
+           label = "paste(Specificity, \" = 0.875\")", parse = T) +
+  annotate("text", 
+           x = 0.125,
+           y = 0.78,
+           label = "paste(Sensitivity, \" = 0.818\")", parse = T) +
+  theme_minimal() +
+  theme(plot.title = element_text(face = "bold", size = 16, hjust = 0.5),
+        axis.title.x = element_text(margin = margin(t = 10)),
+        axis.title.y = element_text(margin = margin(r = 10))) 
 
 # Obtain most significant coefficients as predicted by model
 coef(cv_20, s = cv_20$lambda.min)
 coef_20_min <- coef(cv_20,s = cv_20$lambda.min)[-1,1]
 coef_20_min[coef_20_min!=0] 
+# With 20-fold CV and using min for alpha and lambda, elastic net regression selected 9 relevant variables; 8 are negative and 1 is positive
 
 selected_20  <- sort(abs(coef_20_min[coef_20_min != 0]), decreasing = TRUE)
 selected_20 
-# With 20-fold CV and using min for alpha and lambda, elastic net regression selected 13 relevant variables
-# Notes: AGE has the most influence; all are positive so increasing these by 1 unit increases severity by the corresponding coefficient value
+# Notes: TNF-a has the most influence
 
 # Ranking of top predictors TO-DO: make ggplot version
 par(mar = c(9, 4, 4, 2))
@@ -304,6 +350,8 @@ legend("bottomright",
        legend = c(paste0("10-Fold AUC = ", round(auc(AUC_test_10), 3)),
                   paste0("20-Fold AUC = ", round(auc(AUC_test_20), 3))),
        col = c("navy", "maroon"), lwd = 2, bty = "n")
+
+# For MN: please compare the predictors in the 10-fold and 20-fold models; idk how but show which ones wre exclusive in one and shared in both
 
 # === 4 | STATISTICAL ANALYSIS OF PREDICTORS ======== #TO-DO: check if this is correct
 # Wilcoxon test for age 
@@ -331,10 +379,6 @@ ggplot(data.frame(Age = COV$AGE, Prob = prd_all_10, Severity = factor(COV$Severi
   labs(title = "Age vs Probability of Severe COVID-19",
        x = "Age (years)", y = "Probability of Severity") +
   theme_bw()
-### I don't get this graph, please explain later
-# its like the one we did in another class I made it based on that but idk if it is actually meaningful lol, it just shows that when age increases your probability of severeity decreases, 
-#which is supported by both the correlation thing and your model with age as the only predictor
-
 
 ### I don't think we really need to do this part since she never taught us but I'll leave this here for now
 #I'm also good with getting rid of it i dont think its necessary
@@ -352,13 +396,5 @@ for (i in names(selected_10)) {if (i %in% colnames(X)) {
 
 print(wilcox_results[order(wilcox_results$p_value), ])
 
-# Odds ratio of cytokines (doing with 10 first before 20 to validate)
-odds_10 <- cbind(X[, names(selected_10)], Severity = COV$Severity) 
-odds_10 <- as.data.frame(odds_10)
-model_10 <- glm(Severity ~., data = odds_10, family = "binomial")
-summary(model_10)
-
-or_table <- exp(cbind(OR = coef(model_10), confint(model_10)))
-print(round(or_table, 3))
 
 
